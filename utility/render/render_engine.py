c7b85b8d8824ef1f4ea7913763b4799cf935a750
witspryte <witspryte@gmail.com>
2024-10-23 11:15:15 +0530

    Add files via upload

---
+++ utility/render/render_engine.py
@@ -0,0 +1,75 @@
+import time
+import os
+import tempfile
+import zipfile
+import platform
+import subprocess
+from moviepy.editor import (AudioFileClip, CompositeVideoClip, CompositeAudioClip, ImageClip,
+                            TextClip, VideoFileClip)
+from moviepy.audio.fx.audio_loop import audio_loop
+from moviepy.audio.fx.audio_normalize import audio_normalize
+import requests
+
+def download_file(url, filename):
+    with open(filename, 'wb') as f:
+        response = requests.get(url)
+        f.write(response.content)
+
+def search_program(program_name):
+    try: 
+        search_cmd = "where" if platform.system() == "Windows" else "which"
+        return subprocess.check_output([search_cmd, program_name]).decode().strip()
+    except subprocess.CalledProcessError:
+        return None
+
+def get_program_path(program_name):
+    program_path = search_program(program_name)
+    return program_path
+
+def get_output_media(audio_file_path, timed_captions, background_video_data, video_server):
+    OUTPUT_FILE_NAME = "rendered_video.mp4"
+    magick_path = get_program_path("magick")
+    print(magick_path)
+    if magick_path:
+        os.environ['IMAGEMAGICK_BINARY'] = magick_path
+    else:
+        os.environ['IMAGEMAGICK_BINARY'] = '/usr/bin/convert'
+    
+    visual_clips = []
+    for (t1, t2), video_url in background_video_data:
+        # Download the video file
+        video_filename = tempfile.NamedTemporaryFile(delete=False).name
+        download_file(video_url, video_filename)
+        
+        # Create VideoFileClip from the downloaded file
+        video_clip = VideoFileClip(video_filename)
+        video_clip = video_clip.set_start(t1)
+        video_clip = video_clip.set_end(t2)
+        visual_clips.append(video_clip)
+    
+    audio_clips = []
+    audio_file_clip = AudioFileClip(audio_file_path)
+    audio_clips.append(audio_file_clip)
+
+    for (t1, t2), text in timed_captions:
+        text_clip = TextClip(txt=text, fontsize=100, color="white", stroke_width=3, stroke_color="black", method="label")
+        text_clip = text_clip.set_start(t1)
+        text_clip = text_clip.set_end(t2)
+        text_clip = text_clip.set_position(["center", 800])
+        visual_clips.append(text_clip)
+
+    video = CompositeVideoClip(visual_clips)
+    
+    if audio_clips:
+        audio = CompositeAudioClip(audio_clips)
+        video.duration = audio.duration
+        video.audio = audio
+
+    video.write_videofile(OUTPUT_FILE_NAME, codec='libx264', audio_codec='aac', fps=25, preset='veryfast')
+    
+    # Clean up downloaded files
+    for (t1, t2), video_url in background_video_data:
+        video_filename = tempfile.NamedTemporaryFile(delete=False).name
+        os.remove(video_filename)
+
+    return OUTPUT_FILE_NAME
---
+++ utility/script/script_generator.py
@@ -0,0 +1,61 @@
+import os
+from openai import OpenAI
+import json
+
+if len(os.environ.get("GROQ_API_KEY")) > 30:
+    from groq import Groq
+    model = "mixtral-8x7b-32768"
+    client = Groq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+        )
+else:
+    OPENAI_API_KEY = os.getenv('OPENAI_KEY')
+    model = "gpt-4o"
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+def generate_script(topic):
+    prompt = (
+        """You are a seasoned content writer for a YouTube Shorts channel, specializing in facts videos. 
+        Your facts shorts are concise, each lasting less than 50 seconds (approximately 140 words). 
+        They are incredibly engaging and original. When a user requests a specific type of facts short, you will create it.
+
+        For instance, if the user asks for:
+        Weird facts
+        You would produce content like this:
+
+        Weird facts you don't know:
+        - Bananas are berries, but strawberries aren't.
+        - A single cloud can weigh over a million pounds.
+        - There's a species of jellyfish that is biologically immortal.
+        - Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still edible.
+        - The shortest war in history was between Britain and Zanzibar on August 27, 1896. Zanzibar surrendered after 38 minutes.
+        - Octopuses have three hearts and blue blood.
+
+        You are now tasked with creating the best short script based on the user's requested type of 'facts'.
+
+        Keep it brief, highly interesting, and unique.
+
+        Stictly output the script in a JSON format like below, and only provide a parsable JSON object with the key 'script'.
+
+        # Output
+        {"script": "Here is the script ..."}
+        """
+    )
+
+    response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": topic}
+            ]
+        )
+    content = response.choices[0].message.content
+    try:
+        script = json.loads(content)["script"]
+    except Exception as e:
+        json_start_index = content.find('{')
+        json_end_index = content.rfind('}')
+        print(content)
+        content = content[json_start_index:json_end_index+1]
+        script = json.loads(content)["script"]
+    return script
