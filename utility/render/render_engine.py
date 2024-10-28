7791610157e9b5160eb444398a1f7278452bf742
witspryte <witspryte@gmail.com>
2024-10-28 20:27:18 +0530

    Update render_engine.py

--- utility/render/render_engine.py
+++ utility/render/render_engine.py
@@ -23,7 +23,7 @@ def search_program(program_name):
         return None
 
 def get_program_path(program_name):
-    program_path = search_progimport os
+    program_path = search_progimport
 import tempfile
 from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip, CompositeAudioClip
 
